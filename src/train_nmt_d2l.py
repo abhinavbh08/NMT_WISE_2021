fe19a34ba17e2b4750060113dbc7326d20dd656b
abhinavbh08 <abhinavbhatt.999@gmail.com>
2021-12-26 16:22:25 +0100

    Changes hidden size of transformer

--- src/train_nmt_d2l.py
+++ src/train_nmt_d2l.py
@@ -79,20 +79,20 @@ def train_model(model, data_iter, lr, n_epochs, tgt_vocab, src_vocab,device):
 # embedding_size = 100
 # hidden_size = 200
 # num_layers = 1
-batch_size = 32
+batch_size = 64
 len_sequence = 20
-lr = 0.003
-n_epochs = 30
+lr = 0.005
+n_epochs = 100
 
 data_iter, src_vocab, tgt_vocab = load_data(batch_size, len_sequence)
 # encoder = S2SEncoder(len(src_vocab), embedding_size, hidden_size, num_layers)
 # decoder = S2SAttentionDecoder(len(tgt_vocab), embedding_size, hidden_size, num_layers)
 # model = S2SEncoderDecoder(encoder, decoder)
 encoder = TransformerEncoder(
-    query=32, key=32, value=32, hidden_size=32, num_head=4, dropout=0.1, norm_shape=[32], ffn_input=32, ffn_hidden=64, vocab_size=len(src_vocab), num_layers = 4
+    query=128, key=128, value=128, hidden_size=128, num_head=4, dropout=0.1, norm_shape=[128], ffn_input=128, ffn_hidden=256, vocab_size=len(src_vocab), num_layers = 4
 )
 decoder = TransformerDecoder(
-    query=32, key=32, value=32, hidden_size=32, num_head=4, dropout=0.1, norm_shape=[32], ffn_input=32, ffn_hidden=64, vocab_size=len(tgt_vocab), num_layers = 4
+    query=128, key=128, value=128, hidden_size=128, num_head=4, dropout=0.1, norm_shape=[128], ffn_input=128, ffn_hidden=256, vocab_size=len(tgt_vocab), num_layers = 4
 )
 model = S2SEncoderDecoder(encoder, decoder)
 train_model(model, data_iter, lr, n_epochs, tgt_vocab, src_vocab, device)
